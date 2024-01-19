2e4b700c40b6def6645cdca0db631c25d2d364c7
WHRSTUDIO <125719278+WHRCT22@users.noreply.github.com>
2024-01-19 22:01:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const messageHistory = [];
 
 //自定义启动的WS服务器端口
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8094 });
+const wss = new WebSocket.Server({ port: 8084 });
 // console.log('');
 //定义有WS客户端连接时的操作
 wss.on('connection', function connection(ws, req) {
-    //获取用户远程IP地址，注意！：“connection”方法弃用故改为“remoteAddress”
+    //获取用户远程IP地址
     const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     // 首先发送一条包含“你在线了”的JSON信息给新连接的客户端
@@ -85,56 +85,6 @@ app.get('/WHR-HFS-API/Files-list', (req, res) => {
     });
 });
 
-// 重新启动WS服务器路由
-app.get('/admin/restartws', (req, res) => {
-  wss.close();
-
-  // 创建新的WebSocket服务器
-  wss = new WebSocket.Server({ port: 8094 });
-
-//自定义启动的WS服务器端口
-const WebSocket = require('ws');
-// console.log('');
-//定义有WS客户端连接时的操作
-wss.on('connection', function connection(ws, req) {
-    //获取用户远程IP地址，注意！：“connection”方法弃用故改为“remoteAddress”
-    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    // 首先发送一条包含“你在线了”的JSON信息给新连接的客户端
-    ws.send(JSON.stringify("欢迎加入聊天室！"));
-
-    // 逐条发送messageHistory包含的数组给新连接的客户端
-// 逐条发送messageHistory包含的数组给新连接的客户端，包含用户IP地址
-if (messageHistory.length > 0) {
-  messageHistory.forEach(message => {
-    const messageStringWithIP = `[${message.from}]: ${message.content}`;
-    ws.send(messageStringWithIP);
-  });
-}
-    //此处定义了WS客户端发送信息时的操作
-    ws.on('message', function incoming(message) {
-      console.log(`[WS用户 \x1b[32m${clientIP}\x1b[0m]: \x1b[33m${message}\x1b[0m
-        `);
-        //转换用户发送的二进制数据到文本类型
-        const messageString = message.toString();
-
-        // 将新消息添加到历史消息数组中
-        messageHistory.push({ from: clientIP, content: messageString });
-
-       // 广播消息给所有连接的客户端，带上用户的IP地址
-      wss.clients.forEach(function each(client) {
-  if (client.readyState === WebSocket.OPEN) {
-      const messageWithIP = `[${clientIP}]: ${messageString}`;
-      client.send(messageWithIP);
-  }
-});
-    });
-});
-
-  // 返回成功信息
-  res.send('WebSocket服务器已成功重新启动');
-});
-
 // 下载页面路由
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -157,7 +107,7 @@ const storage = multer.diskStorage({
   });
   
   const fileFilter = (req, file, callback) => {
-    // 解决中文名乱码的问题 latin1 是一种编码格式
+    // 解决中文名乱码的问题 使用latin1编码格式
     file.originalname = Buffer.from(file.originalname, "latin1").toString(
       "utf8"
     );
@@ -219,7 +169,7 @@ const deleteFileMiddleware = (req, res, next) => {
 
 
 // 反向代理 WebSocket 后端至 /websocket
-const wsport = 8094;
+const wsport = 8084;
 const websocketurl = `/websocket`;
 app.use(`${websocketurl}`, createProxyMiddleware({
   target: `ws://localhost:${wsport}`, // 替换 8084 为自定义端口
@@ -227,8 +177,9 @@ app.use(`${websocketurl}`, createProxyMiddleware({
   logLevel: 'silent'
 }));
 
-// 启动服务
+// 启动服务端口
 const port = 11452
+// 显示部分
 app.listen({port}, '0.0.0.0', () => {
     console.log(`
     ╦ ╦╦ ╦╦═╗   ╦ ╦╔═╗╔═╗  ╦╔═╗  ╦═╗╦ ╦╔╗╔╔╗╔╦╔╗╔╔═╗         
